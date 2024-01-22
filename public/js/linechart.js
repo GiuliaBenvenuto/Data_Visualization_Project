a76ed08110ad39b1ff9eee58adedae5fa2557728
GiuliaBenvenuto <giulia.benvenuto@outlook.com>
2024-01-22 18:19:47 +0100

    Not available data managed on linechart

--- public/js/linechart.js
+++ public/js/linechart.js
@@ -1,44 +1,44 @@
 export function updateLinechart(checkedValues) {
 
     const countryMapping = {
-        "AL": "Albania",
-        "AT": "Austria",
-        "BE": "Belgium",
-        "BG": "Bulgaria",
-        "CY": "Cyprus",
-        "CZ": "Czechia",
-        "DE": "Germany",
-        "DK": "Denmark",
-        "EE": "Estonia",
-        "EL": "Greece",
-        "ES": "Spain",
-        "EU28": "European Union",
-        "FI": "Finland",
-        "FR": "France",
-        "HR": "Croatia",
-        "HU": "Hungary",
-        "IE": "Ireland",
-        "IS": "Iceland",
-        "IT": "Italy",
-        "LT": "Lithuania",
-        "LU": "Luxembourg",
-        "LV": "Latvia",
-        "ME": "Montenegro",
-        "MK": "North Macedonia",
-        "MT": "Malta",
-        "NL": "Netherlands",
-        "NO": "Norway",
-        "PL": "Poland",
-        "PT": "Portugal",
-        "RO": "Romania",
-        "RS": "Serbia",
-        "SE": "Sweden",
-        "SI": "Slovenia",
-        "SK": "Slovakia",
-        "TR": "Türkiye",
-        "UK": "United Kingdom",
-        "XK": "Kosovo*"
-    };
+        'AL': {'name': 'Albania', 'color': '#FF6347'},
+        'AT': {'name': 'Austria', 'color': '#4682B4'},
+        'BE': {'name': 'Belgium', 'color': '#32CD32'},
+        'BG': {'name': 'Bulgaria', 'color': '#FFD700'},
+        'CY': {'name': 'Cyprus', 'color': '#6A5ACD'},
+        'CZ': {'name': 'Czechia', 'color': '#FF69B4'},
+        'DE': {'name': 'Germany', 'color': '#8B4513'},
+        'DK': {'name': 'Denmark', 'color': '#9ACD32'},
+        'EE': {'name': 'Estonia', 'color': '#4169E1'},
+        'EL': {'name': 'Greece', 'color': '#8A2BE2'},
+        'ES': {'name': 'Spain', 'color': '#FF8C00'},
+        'EU28': {'name': 'European Union', 'color': '#2E8B57'},
+        'FI': {'name': 'Finland', 'color': '#C71585'},
+        'FR': {'name': 'France', 'color': '#DC143C'},
+        'HR': {'name': 'Croatia', 'color': '#008B8B'},
+        'HU': {'name': 'Hungary', 'color': '#BDB76B'},
+        'IE': {'name': 'Ireland', 'color': '#556B2F'},
+        'IS': {'name': 'Iceland', 'color': '#9932CC'},
+        'IT': {'name': 'Italy', 'color': '#8B0000'},
+        'LT': {'name': 'Lithuania', 'color': '#E9967A'},
+        'LU': {'name': 'Luxembourg', 'color': '#483D8B'},
+        'LV': {'name': 'Latvia', 'color': '#2F4F4F'},
+        'ME': {'name': 'Montenegro', 'color': '#00CED1'},
+        'MK': {'name': 'North Macedonia', 'color': '#9400D3'},
+        'MT': {'name': 'Malta', 'color': '#FF4500'},
+        'NL': {'name': 'Netherlands', 'color': '#98FB98'},
+        'NO': {'name': 'Norway', 'color': '#AFEEEE'},
+        'PL': {'name': 'Poland', 'color': '#DB7093'},
+        'PT': {'name': 'Portugal', 'color': '#CD853F'},
+        'RO': {'name': 'Romania', 'color': '#FFC0CB'},
+        'RS': {'name': 'Serbia', 'color': '#DDA0DD'},
+        'SE': {'name': 'Sweden', 'color': '#BC8F8F'},
+        'SI': {'name': 'Slovenia', 'color': '#4169E1'},
+        'SK': {'name': 'Slovakia', 'color': '#8B4513'},
+        'TR': {'name': 'Türkiye', 'color': '#FA8072'},
+        'UK': {'name': 'United Kingdom', 'color': '#A0522D'},
+        'XK': {'name': 'Kosovo*', 'color': '#C0C0C0'}
+    }
     
 
     let countries = checkedValues;
@@ -56,7 +56,6 @@ export function updateLinechart(checkedValues) {
         .attr("height", height + margin.top + margin.bottom)
     .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
 
 
     d3.csv('./csv/linechart_processed.csv', function(data) {
@@ -133,28 +132,63 @@ export function updateLinechart(checkedValues) {
         // Function to process data for a given country
         function processDataForCountry(countryCode) {
             var countryData = data.filter(function(d) { return d.geo === countryCode; })[0];
-
+        
             return yearColumns.map(year => {
-                return { date: year, value: +countryData[year] };
+                // Check if the value is not a number (NaN) or is ":" and replace it with 0
+                var value = countryData[year];
+                if (value === ":" || isNaN(value)) {
+                    value = 0;
+                } else {
+                    // Ensure the value is a number
+                    value = +value;
+                }
+        
+                return { date: year, value: value };
             });
         }
+        
 
-        // Function to add a line to the chart
-        function addLineToChart(processedData, color) {
+        /* Function to add a line to the chart
+        function addLineToChart(processedData, country) {
             // console.log("Processed Data:", processedData);
             svg.append("path")
                 .datum(processedData)
                 .attr("fill", "none")
-                .attr("stroke", color)
+                .attr("stroke", countryMapping[country]["color"])
                 .attr("stroke-width", 1.5)
                 .attr("d", d3.line()
                     .x(function(d) { return x(d.date); })
                     .y(function(d) { return y(d.value); })
                 );
+        } */
+        function addLineToChart(processedData, country) {
+            // Custom line generator
+            var lineGenerator = d3.line()
+                .defined(function(d) { return d.value !== 0; }) // Define where the line is drawn
+                .x(function(d) { return x(d.date); })
+                .y(function(d) { return y(d.value); });
+        
+            // Draw the line for defined areas
+            svg.append("path")
+                .datum(processedData)
+                .attr("fill", "none")
+                .attr("stroke", countryMapping[country]["color"])
+                .attr("stroke-width", 1.5)
+                .attr("d", lineGenerator);
+        
+            // Draw a dotted line for undefined areas
+            svg.append("path")
+                .datum(processedData)
+                .attr("fill", "none")
+                .attr("stroke", countryMapping[country]["color"])
+                .attr("stroke-width", 1.5)
+                .attr("stroke-dasharray", "3,3") // This creates the dotted effect
+                .attr("d", lineGenerator.defined(function(d) { return d.value === 0; })); // Define where the dotted line is drawn
         }
+        
 
         // Function to add a dots to the chart
-        function addDotsToChart(processedData, color, country) {
+        function addDotsToChart(processedData, country) {
             console.log("Processed Data:", processedData);
             svg.append("g")
             .selectAll("dot")
@@ -163,18 +197,20 @@ export function updateLinechart(checkedValues) {
             .append("circle")
                 .attr("cx", function(d) { return x(d.date) } )
                 .attr("cy", function(d) { return y(d.value) } )
-                .attr("r", 4)
-                .attr("fill", color)
+                .attr("r", 5)
+                .attr("fill", countryMapping[country]["color"])
                 .attr("stroke", "#fff")
 
             .on("mouseover", function (d) {
                 tooltip.transition()
                     .duration(100)
                     .style("opacity", 0.9);
-                tooltip.html(
-                    "<span style='color: #333;'> <strong>State: </strong> " + countryMapping[country] + "</span><br>" + 
-                    "<span style='color: #333;'> <strong>Percentage: </strong> " + d.value + "%</span><br>"
-                )
+                    tooltip.html(
+                        "<span style='color: #333;'> <strong>State: </strong> " + countryMapping[country]["name"] + "</span><br>" + 
+                        "<span style='color: #333;'> <strong>Percentage: </strong> " + 
+                        (d.value === 0 ? "Data not available" : d.value + "%") + "</span><br>"
+                    )
+                    
                 .style("left", (d3.event.pageX + 10) + "px")
                 .style("top", (d3.event.pageY - 28) + "px");
             })
@@ -191,10 +227,10 @@ export function updateLinechart(checkedValues) {
             // console.log("Country Data Processed:", countryDataProcessed);
 
             // Assign a unique color for each line
-            var color = d3.schemeCategory10[index % 10]; // Change or expand this for more than 10 countries
+            // var color = d3.schemeCategory10[index % 10]; // Change or expand this for more than 10 countries
 
-            addLineToChart(countryDataProcessed, color);
-            addDotsToChart(countryDataProcessed, color, country);
+            addLineToChart(countryDataProcessed, country);
+            addDotsToChart(countryDataProcessed, country);
         });
             
         // Add horizontal grid lines
