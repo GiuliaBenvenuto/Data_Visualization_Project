a14faa8b6813af64c6fb45028bbfc3cdeb67f4ab
GiuliaBenvenuto <giulia.benvenuto@outlook.com>
2024-01-27 15:57:42 +0100

    Year selection on Map and legend
    Added the four buttons to switch year on the Map, added the legend and the on hover effect, added the color scale.

--- public/js/map.js
+++ public/js/map.js
@@ -1,159 +1,235 @@
 
-  var margin = {top: 50, right: 50, bottom: 50, left: 50},
-    width = 1000 - margin.left - margin.right,
-    height = 1000 - margin.top - margin.bottom;
+  export function updateMap(year) {
+    console.log("SELECTED YEAR js:", year);
 
-  // The svg
-  var svg = d3.select("#my_map")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var csvUrl = "./csv/map_processed_" + year + ".csv"
+    var geoJsonUrl = "./json/europe.geojson"
+    console.log("CSV URL:", csvUrl);
+    // var csvUrl = "./csv/map_processed_2015.csv"
 
-
-  // Map and projection
-  var path = d3.geoPath();
-  var projection = d3.geoMercator()
-    .center([13, 52])
-    .scale(600)
-    .translate([width / 2, height / 2]);
+    // Select the chart container and clear its content
+    var mapContainer = d3.select("#my_map");
+    mapContainer.selectAll("*").remove(); 
 
 
+    var margin = {top: 200, right: 50, bottom: 50, left: 50},
+      width = 800 - margin.left - margin.right,
+      height = 800 - margin.top - margin.bottom;
 
-  /*
-  var colorScale = d3.scaleThreshold()
-  .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
-  .range(d3.schemeBlues[7]); */
+    // The svg
+    var svg = d3.select("#my_map")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  var colorScale = d3.scaleLinear();
-  colorScale.domain([0, 60])
-  colorScale.range(['#bbf7d0', '#15803d']) 
 
-  var populationData = {};
+    // Map and projection
+    var projection = d3.geoMercator()
+      .center([13, 52])
+      .scale(600)
+      .translate([width / 2, height / 2]);
 
-  var geoJsonUrl = "./json/europe.geojson"
-  var csvUrl = "./csv/map_processed_2015.csv"
 
-  // Load external data and boot
-  d3.queue()
-    .defer(d3.json, geoJsonUrl)
-    .defer(d3.csv, csvUrl, function(d) {
-        // Store population data with country code as key
-        if(d.pop !== "") {
-            populationData[d.code] = +d.pop;
-        }
-    })
-    .await(ready);
-    // .defer(d3.csv, csvUrl, function(d) { data.set(d.code, +d.pop); })
+    // var colorScale = d3.scaleSequential(d3.interpolateRdBu)
+    var colorScale = d3.scaleSequential(d3.interpolateRdYlGn)
+      .domain([0, 65]);
+    
+      // MAX 2015: 55.89
+      // MAX 2016: 54.15
+      // MAX 2017: 58.31
+      // MAX 2019: 61.57
 
-  console.log("DATA MAP", populationData)
+      // LEGEND
+      var legendWidth = 90;
+      var legendSpacing = 25;
+      var legendX = margin.left - legendWidth; // Adjust the X position as needed
+      var legendY = margin.top - 400; // Adjust the Y position as needed
 
-  function ready(error, topo) {
-  if (error) throw error;
+      // Create a group for the legend
+      var legendGroup = svg.append("g")
+        .attr("class", "legend")
+        .attr("transform", "translate(" + legendX + "," + legendY + ")");
 
-  let mouseOver = function(d) {
-    d3.selectAll(".Country")
-      .transition()
-      .duration(200)
-      .style("opacity", .5);
+      // Data for legend items (5 items)
+      var legendData = [
+        { color: colorScale(0), label: "0% - 13%" },
+        { color: colorScale(13), label: "14% - 26%" },
+        { color: colorScale(26), label: "27% - 39%" },
+        { color: colorScale(39), label: "40% - 52%" },
+        { color: colorScale(65), label: "53% - 65%" }, 
+        { color: "url(#stripes)", label: "No data" }
+      ];
 
-    // Highlight the border of the hovered country
-    d3.select(this)
-      .transition()
-      .duration(200)
-      .style("opacity", 1)
-      .style("stroke", "black") // Ensure this is visible against the map
-      .style("stroke-width", "2px"); // Adjust the width as needed
-  }
-
-  let mouseLeave = function(d) {
-    d3.selectAll(".Country")
-      .transition()
-      .duration(200)
-      .style("opacity", .8);
-
-    // Reset the border of the hovered country
-    d3.select(this)
-      .transition()
-      .duration(200)
-      .style("stroke", "none")
-      .style("stroke-width", "0px");  // Reset the stroke width
-  }
-
-  // Tooltip
-  var tooltip = d3.select('body')
-  .append("div")
-  .style("position", "absolute")
-  .style("background", "#f0f0f0") // Use a light grey color for the background
-  .style("padding", "10px")
-  .style("border", "1px solid #ccc") // Use a darker grey for the border
-  .style("border-radius", "8px")
-  .style("pointer-events", "none")
-  .style("opacity", 0)
-  .style("font", "15px Montserrat")
-  .style("color", "#333");
-
-  // Function to show the tooltip
-  function showTooltip(d) {
-    tooltip
-      .transition()
-      .duration(100)
-      .style("opacity", 0.9);
-    tooltip
-    .html("<strong>Country: </strong>" + (d.properties.NAME) + "<br><strong>Percentage: </strong> " + 
-    (d.total ? d.total + "%" : "No data")
-    )
-
-      .style("left", (d3.event.pageX + 10) + "px")
-      .style("top", (d3.event.pageY - 28) + "px");
-  }
-
-  // Function to move the tooltip
-  function moveTooltip(d) {
-    tooltip
-      .style("left", (d3.event.pageX + 10) + "px")
-      .style("top", (d3.event.pageY - 28) + "px");
-  }
-
-  // Function to hide the tooltip
-  function hideTooltip(d) {
-    tooltip
-      .transition()
-      .duration(100)
-      .style("opacity", 0);
-  }
-
-  // Draw the map
-  svg.append("g")
-        .selectAll("path")
-        .data(topo.features)
+      // Create legend items
+      var legendItems = legendGroup.selectAll(".legend-item")
+        .data(legendData)
         .enter()
-        .append("path")
-        .attr("d", d3.geoPath().projection(projection))
-        .attr("fill", function (d) {
-          // Use ISO3 code from GeoJSON properties to get the population data
-          d.total = populationData[d.properties.ISO3] || 0;
-          return colorScale(d.total);
+        .append("g")
+        .attr("class", "legend-item")
+        .attr("transform", function (d, i) {
+          return "translate(0," + (i * legendSpacing) + ")";
+        });
+
+      // Add colored squares
+      legendItems
+        .append("rect")
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", function (d) { return d.color; })
+        .style("stroke", "#293241") // Add a black border
+        .style("stroke-width", 1);
+
+      // Add labels
+      legendItems
+        .append("text")
+        .attr("x", 25)
+        .attr("y", 10)
+        .attr("dy", ".35em")
+        .style("text-anchor", "start")
+        .style("color", "#293241")
+        .style("font", "18px Montserrat")
+        .text(function (d) { return d.label; });
+
+
+
+    var populationData = {};
+    // Load external data and boot
+    d3.queue()
+      .defer(d3.json, geoJsonUrl)
+      .defer(d3.csv, csvUrl, function(d) {
+          // Store population data with country code as key
+          if(d.pop !== "") {
+              populationData[d.code] = +d.pop;
+          }
       })
-      /*
-        .attr("fill", function (d) {
-            // Get population data
-            d.total = populationData[d.id] || 0;
-            return colorScale(d.total);
-        })*/
-      .style("stroke", "transparent")
-      .attr("class", function(d){ return "Country" } )
+      .await(ready);
+
+    console.log("DATA MAP", populationData)
+
+    function ready(error, topo) {
+      if (error) throw error;
+
+      let mouseOver = function(d) {
+        d3.selectAll(".Country")
+          .transition()
+          .duration(200)
+          .style("opacity", .5);
+
+        // Highlight the border of the hovered country
+        d3.select(this)
+          .transition()
+          .duration(200)
+          .style("opacity", 1.5)
+          .style("stroke", "black") // Ensure this is visible against the map
+          .style("stroke-width", "1.5px"); // Adjust the width as needed
+      }
+
+      let mouseLeave = function(d) {
+        d3.selectAll(".Country")
+          .transition()
+          .duration(200)
+          .style("opacity", .8);
+
+        // Reset the border of the hovered country
+        d3.select(this)
+          .transition()
+          .duration(200)
+          .style("stroke", "black")
+          .style("stroke-width", "1px");  // Reset the stroke width
+      }
+
+      // Tooltip
+      var tooltip = d3.select('body')
+      .append("div")
+      .style("position", "absolute")
+      .style("background", "#f0f0f0") // Use a light grey color for the background
+      .style("padding", "10px")
+      .style("border", "1px solid #ccc") // Use a darker grey for the border
+      .style("border-radius", "8px")
+      .style("pointer-events", "none")
+      .style("opacity", 0)
+      .style("font", "15px Montserrat")
+      .style("color", "#333");
+
+      // Function to show the tooltip
+      function showTooltip(d) {
+        tooltip
+          .transition()
+          .duration(100)
+          .style("opacity", 0.9);
+        tooltip
+        .html("<strong>Country: </strong>" + (d.properties.NAME) + "<br><strong>Percentage: </strong> " + 
+          (d.total ? d.total + "%" : "No data")
+        )
+
+          .style("left", (d3.event.pageX + 10) + "px")
+          .style("top", (d3.event.pageY - 28) + "px");
+      }
+
+      // Function to move the tooltip
+      function moveTooltip(d) {
+        tooltip
+          .style("left", (d3.event.pageX + 10) + "px")
+          .style("top", (d3.event.pageY - 28) + "px");
+      }
+
+      // Function to hide the tooltip
+      function hideTooltip(d) {
+        tooltip
+          .transition()
+          .duration(100)
+          .style("opacity", 0);
+      }
+
+      // Define the striped pattern inside your SVG
+      svg.append("defs")
+      .append("pattern")
+        .attr("id", "stripes")
+        .attr("width", 4)
+        .attr("height", 4)
+        .attr("patternUnits", "userSpaceOnUse")
+        .attr("patternTransform", "rotate(45)")
+      .append("rect")
+        .attr("width", 2)
+        .attr("height", 4)
+        .attr("transform", "translate(0,0)")
+        .attr("fill", "white"); // Color of the stripes
+
+      // Draw the map with modifications for null, NaN, or zero values
+      svg.append("g")
+      .selectAll("path")
+      .data(topo.features)
+      .enter()
+      .append("path")
+      .attr("d", d3.geoPath().projection(projection))
+      .attr("fill", function (d) {
+        // Use ISO3 code from GeoJSON properties to get the population data
+        d.total = populationData[d.properties.ISO3];
+
+        // Check for null, NaN, or zero values and apply the striped pattern
+        if (d.total === null || isNaN(d.total) || d.total === 0) {
+          return "url(#stripes)"; // Use the defined striped pattern
+        } else {
+          return colorScale(d.total); // Use the color scale for other values
+        }
+      })
+      .style("stroke", "black")
+      .style("stroke-width", "1px")
+      .attr("class", function(d){ return "Country"; })
       .style("opacity", 1)
 
-      svg.selectAll(".Country")
-      .on("mouseover", function(d) {
-        mouseOver.call(this, d);
-        showTooltip(d);
-      })
-      .on("mousemove", moveTooltip)
-      .on("mouseleave", function(d) {
-        mouseLeave.call(this, d);
-        hideTooltip(d);
-      });
+        svg.selectAll(".Country")
+        .on("mouseover", function(d) {
+          mouseOver.call(this, d);
+          showTooltip(d);
+        })
+        .on("mousemove", moveTooltip)
+        .on("mouseleave", function(d) {
+          mouseLeave.call(this, d);
+          hideTooltip(d);
+        });
+
     }
+  }
---
+++ public/js/wordcloud.js
@@ -0,0 +1,45 @@
+// List of words
+var myWords = [{word: "Running", size: "10"}, {word: "Surfing", size: "20"}, {word: "Climbing", size: "50"}, {word: "Kiting", size: "30"}, {word: "Sailing", size: "20"}, {word: "Snowboarding", size: "60"} ]
+
+
+// set the dimensions and margins of the graph
+var margin = {top: 0, right: 0, bottom: 0, left: 0},
+    width = 1000 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#my_wordcloud").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+// Use your list of words and sizeScale as defined
+var layout = d3.layout.cloud()
+    .size([width, height])
+    .words(myWords.map(function(d) {
+      return {text: d.word, size: +d.size}; // Ensure size is numeric
+    }))
+    .padding(5)
+    .rotate(function() { return ~~(Math.random() * 2) * 90; })
+    .font("Impact")
+    .fontSize(function(d) { return d.size; })
+    .on("end", draw);
+
+layout.start();
+
+function draw(words) {
+    svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+        .selectAll("text")
+        .data(words)
+        .enter().append("text")
+        .style("font-size", function(d) { return d.size + "px"; })
+        .style("font-family", "Impact")
+        .attr("text-anchor", "middle")
+        .attr("transform", function(d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+}
